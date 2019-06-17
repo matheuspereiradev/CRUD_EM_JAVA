382114ab427cd444718903033b79dec5d3074398
Matheus <Matheus@MATHEUS-LIMA>
2019-06-16 22:48:40 -0300

    verificação do email

--- src/sistema/telas/CadastrarFuncionario.java
+++ src/sistema/telas/CadastrarFuncionario.java
@@ -20,6 +20,8 @@ import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.*;
 import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
@@ -186,12 +188,21 @@ public class CadastrarFuncionario extends JPanel {
             return;
         }
 
+        Boolean emailValido = false;
+        String ePatern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        Pattern p = Pattern.compile(ePatern);
+        Matcher m = p.matcher(tfEmail.getText());
+        emailValido = m.matches();
+
+        if (!emailValido) {
+            JOptionPane.showMessageDialog(null, "Insira um email válido");
+            return;
+        }
+
         // conexão
         Connection conexao;
-        // instrucao SQL
-        //Statement instrucaoSQL;
-        // resultados
-        ResultSet resultados;
+
+        //intruçao sql
         PreparedStatement instrucaoSQL;
 
         try {
